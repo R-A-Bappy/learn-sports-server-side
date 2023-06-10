9e17ae2413753ad51aa800d76c05ea9cbe98e8cd
Bappy <mdbappy89599@gmail.com>
2023-06-10 12:42:26 +0600

    class add on database

--- index.js
+++ index.js
@@ -2,10 +2,63 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pr8kelc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const classesCollection = client.db('learnSportsDB').collection('classes');
+
+        app.post('/instructor/addClass', async (req, res) => {
+            const classData = req.body;
+            const result = await classesCollection.insertOne(classData);
+            res.send(result);
+        })
+
+        app.get('/instructor/classes/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = { instructorEmail: email };
+            const result = await classesCollection.find(query).toArray();
+            console.log(result);
+            res.send(result);
+        })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
